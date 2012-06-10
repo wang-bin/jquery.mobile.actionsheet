c2aa6e7302d75276f311fe4fe5df0b5be56d6de8
WangBin <wbsecg1@gmail.com>
2012-06-10 20:59:22 +0800

    add callbacks for ok and cancel. page center

--- jquery.mobile.actionsheet.js
+++ jquery.mobile.actionsheet.js
@@ -9,28 +9,47 @@
  */
 (function($,window){
 	$.widget("mobile.actionsheet",$.mobile.widget,{
+		options: {
+			ok: undefined,
+			cancel: undefined
+		},
+		ok_call: undefined,
+		cancel_call: undefined,
 		wallpaper: undefined,
 		content: undefined,
 		_init: function() {
+			this.ok_call = this.options.ok;
+			this.cancel_call = this.options.cancel;
+			//console.log("ok: "+this.ok_call);
+			//console.log("cancel: "+this.cancel_call);
+			
 			var self = this;
+			//console.log(this.element.jqmData('sheet'));
 			this.content = ((typeof this.element.jqmData('sheet') !== 'undefined')
 				? $('#' + this.element.jqmData('sheet'))
-				: this.element.next('div')).addClass('ui-actionsheet-content');
+				: this.element.next('div')).addClass('ui-actionsheet-content'); //TODO: custom widget
 			// Move content to parent page
 			// Otherwise there is an error i will describe here soon
 			var parentPage = this.element.parents(':jqmData(role="page")');
-			this.content.remove().appendTo(parentPage);
+			this.content.remove().appendTo(parentPage).hide();
 
 			//setup command buttons
-			this.content.find(':jqmData(role="button")').filter(':jqmData(rel!="close")')
-				.addClass('ui-actionsheet-commandbtn')
+			/*
+			this.content.find(':jqmData(role="button")').filter(':jqmData(rel!="ok")').filter(':jqmData(rel!="cancel")')
+				.addClass('ui-actionsheet-commandbtn');*/
+			//setup ok button
+			this.content.find(':jqmData(rel="ok")').addClass('ui-actionsheet-closebtn')
 				.bind('click', function(){
-					self.reset();
+				console.log('ok');
+					if (self.ok_call !== undefined)
+						if (self.ok_call() == false)
+							return;
+					self.close();
 				});
-			//setup close button
-			this.content.find(':jqmData(rel="close")')
-				.addClass('ui-actionsheet-closebtn')
+			this.content.find(':jqmData(rel="cancel")').addClass('ui-actionsheet-closebtn')
 				.bind('click', function(){
+					if (self.cancel_call !== undefined)
+						self.cancel_call();
 					self.close();
 				});
 			this.element.bind('click', function(){
@@ -41,17 +60,14 @@
 			}
 		},
 		open: function() {
+			if(this.wallpaper !== undefined) //this function will call twice if call actionsheet in js. why? init twice?
+				this.wallpaper.remove();
 			this.element.unbind('click'); //avoid twice opening
-			
-			var cc= this.content.parents(':jqmData(role="page")');
-			this.wallpaper= $('<div>', {'class':'ui-actionsheet-wallpaper'})
-				.appendTo(cc)
-				.show();
+			var cc = this.content.parents(':jqmData(role="page")');
+			this.wallpaper = $('<div>', {'class':'ui-actionsheet-wallpaper'}).appendTo(cc).show();
 			
 			//window.setTimeout($.proxy(this._wbc, this), 500);
-			this.wallpaper.bind(
-					"click",
-					$.proxy(function() { this.close(); },this));
+			this.wallpaper.bind("click",$.proxy(function(e) { e.preventDefault();/*this.close();*/ },this));
 			this._positionContent();
 
 			$(window).bind('orientationchange.actionsheet',$.proxy(function () {
@@ -87,19 +103,15 @@
 		},
 		reset: function() {
 			this.wallpaper.remove();
-			this.content
-				.removeClass("ui-actionsheet-animateOut")
-				.removeClass("ui-actionsheet-animateIn")
-				.hide();
+			this.content.removeClass("ui-actionsheet-animateOut").removeClass("ui-actionsheet-animateIn").hide();
 			var self= this;
-			this.element.bind('click', function(){
-				self.open();
-			});
+			this.element.bind('click', function(){self.open();});
 		},
 		_positionContent: function() {
 			var height = $(window).height(),
-				width = $(window).width(),
+				width = $.mobile.activePage.width(), //page center, not window center
 				scrollPosition = $(window).scrollTop();
+			//console.log("h: " + height + " w: " + width + " s: " +scrollPosition);
 			this.content.css({
 				'top': (scrollPosition + height / 2 - this.content.height() / 2),
 				'left': (width / 2 - this.content.width() / 2)
